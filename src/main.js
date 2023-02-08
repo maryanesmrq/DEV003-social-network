f2ec1040d042817b26451465748794335c0d6a78
maryanesmrq <maryan.manrique@gmail.com>
2023-02-07 18:12:34 -0600

    readme changes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 // Este es el punto de entrada de tu aplicacion
+import { home } from './components/home.js';
+import { register } from './components/register.js';
+import { login } from './components/login.js';
+// import { myFunction } from './lib/index.js';
 
-import { myFunction } from './lib/index.js';
+// acceder al nodo
+const rootDiv = document.getElementById('root');
 
-myFunction();
+// funcion para las rutas
+const routes = {
+  '/': home,
+  '/register': register,
+  '/login': login,
+};
+
+const component = routes[window.location.pathname];
+// insertando appendChild
+rootDiv.appendChild = (component);
+// myFunction();
